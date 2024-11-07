deed17b4ef36e5347c91d4ecd69e327a667593ec
Benie-braine <eldonnziengui@gmail.com>
2024-11-07 19:54:59 +0100

    navbar Ajouter

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Navbar from '../Components/Navbar/Navbar';
 function Accueil() {
   return (
     <div>
-      Accueil
+     <Navbar/>  
     </div>
+    
   );
 }
 
