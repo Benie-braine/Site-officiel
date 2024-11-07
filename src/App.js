92fefac0234dc195b6d0bbbee0cd7c40a7a506f5
Benie-braine <eldonnziengui@gmail.com>
2024-11-07 18:45:36 +0100

    dependances installées

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Accueil from './Pages/Accueil';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+         
+        </Routes>
+      </Router> 
     </div>
   );
 }
