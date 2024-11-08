3c37ba41b155ebcc51e8fec6556a5dc134e404f1
chrissergio96 <safou.christiansergio@gmail.com>
2024-11-08 16:37:31 +0100

    Header

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
+import Header from '../Components/Header/Header';
 function Accueil() {
   return (
     <div>
      <Navbar/>  
+     <Header/>
     </div>
     
   );
