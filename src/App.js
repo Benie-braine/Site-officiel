bdea040f6e03de0c63b75f9c4540e8603f5e2098
chrissergio96 <safou.christiansergio@gmail.com>
2024-11-22 11:30:47 +0100

    verification

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Accueil from './Pages/Accueil.jsx';
-import Etatcivil from './Components/Etat-civil/Etatcivil';
-import Urbanisme from './Pages/Urbanisme.jsx';
-import Amenagements from './Pages/Amenagements.jsx'
+// import Etatcivil from './Components/Etat-civil/Etatcivil';
+// import Urbanisme from './Pages/Urbanisme.jsx';
+// import Amenagements from './Pages/Amenagements.jsx';
+import ReglesUrb from './Pages/ReglesUrb.jsx'
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -17,9 +19,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Accueil />} />
-          <Route path="/" element={<Etatcivil />} />
-          <Route path="/" element={<Urbanisme />} />
-          <Route path="/" element={<Amenagements />} />
+          {/* <Route path="/" element={<Etatcivil />} /> */}
+          {/* <Route path="/" element={<Urbanisme />} /> */}
+          {/* <Route path="/" element={<Amenagements />} /> */}
+          <Route path="/" element={<ReglesUrb />} />
 
         </Routes>
       </Router> 
--- src/ComponentsUrbanisme/RubriqueUrb/RubriqueUrb.jsx
+++ src/ComponentsUrbanisme/RubriqueUrb/RubriqueUrb.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './RubriqueUrb.css';
 import log from '../../Images/gg_arrow-up-o.png';
-import { Link } from 'react-router-dom';
 
 const RubriqueUrb = () => {
     return (
@@ -9,18 +8,14 @@ const RubriqueUrb = () => {
            <h2>Rubriques</h2>
 
            <div>
-            <Link to='/Amenagements'>
             <button className='travaux'>
            <img src={log} alt="gg_arrow-up-o.png" />
            <h4>Aménagements</h4>
            </button>
-           </Link>
-           <Link to='/ReglesUrb'>
            <button className='travaux'>
            <img src={log} alt="gg_arrow-up-o.png" />
            <h4>Règles d'urbanisme</h4>
            </button>
-           </Link>
            <button className='travaux'>
            <img src={log} alt="gg_arrow-up-o.png" />
            <h4>Autorisations d'urbanisme</h4>
