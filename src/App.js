492fc8c6f09053d3b5f162b506c392f25d9ace94
Benie-braine <eldonnziengui@gmail.com>
2024-11-25 10:00:59 +0100

    fresh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
- import Accueil from './Pages/Accueil';
- import Etatcivil from './Pages/Etatcivil';
- import Urbanisme from './Pages/Urbanisme.jsx';
-=======
 import Accueil from './Pages/Accueil.jsx';
-// import Etatcivil from './Components/Etat-civil/Etatcivil';
+import Etatcivil from './Pages/Etatcivil';
 import Urbanisme from './Pages/Urbanisme.jsx';
 import Amenagements from './Pages/Amenagements.jsx';
-import ReglesUrb from './Pages/ReglesUrb.jsx'
+import ReglesUrb from './Pages/ReglesUrb.jsx';
+import Légalisation from './Pages/Légalisation';
 
->>>>>>> 18eabbdf6452da99b29906f400478da6dccc1c53
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Légalisation from './Pages/Légalisation';
-
 
 function App() {
   return (
@@ -26,21 +19,15 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Accueil />} />
-<<<<<<< HEAD
-          <Route path="/" element={<Etatcivil />} />
-           <Route path="/" element={<Légalisation />} />
-             <Route path="/" element={<Urbanisme />} />
-=======
-          {/* <Route path="/etatcivil" element={<Etatcivil />} /> */}
+          <Route path="/etatcivil" element={<Etatcivil />} />
+          <Route path="/legalisation" element={<Légalisation />} />
           <Route path="/urbanisme" element={<Urbanisme />} />
           <Route path="/amenagements" element={<Amenagements />} />
           <Route path="/reglesurb" element={<ReglesUrb />} />
-
->>>>>>> 18eabbdf6452da99b29906f400478da6dccc1c53
         </Routes>
-      </Router> 
+      </Router>
     </div>
   );
-} 
+}
 
 export default App;
--- src/Légalisation/info.jsx
+++ src/Légalisation/info.jsx
@@ -5,7 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { image } from '../Images/actes de Naissances ..............jpg';
 
 export default function MediaCard() {
   return (
