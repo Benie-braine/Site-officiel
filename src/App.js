7bd20c58354410938d185a7d5c4aa0fd892cefc9
chrissergio96 <safou.christiansergio@gmail.com>
2024-11-25 12:40:03 +0100

    cool

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import ScrollToTop from './ComponentsUrbanisme/ScrollToTop/ScrollToTop.jsx';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Accueil from './Pages/Accueil.jsx';
+import Etatcivil from './Pages/Etatcivil'; // Garder cette ligne
 import Urbanisme from './Pages/Urbanisme.jsx';
 import Amenagements from './Pages/Amenagements.jsx';
-import ReglesUrb from './Pages/ReglesUrb.jsx'
+import ReglesUrb from './Pages/ReglesUrb.jsx';
+import Légalisation from './Pages/Légalisation'; // Garder cette ligne
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
 
 function App() {
   return (
@@ -20,12 +21,13 @@ function App() {
       <ScrollToTop /> {/* Ce composant s'assure que chaque navigation commence en haut */}
         <Routes>
           <Route path="/" element={<Accueil />} />
+          <Route path="/etatcivil" element={<Etatcivil />} /> {/* Garder cette ligne */}
+          <Route path="/legalisation" element={<Légalisation />} /> {/* Garder cette ligne */}
           <Route path="/urbanisme" element={<Urbanisme />} />
           <Route path="/amenagements" element={<Amenagements />} />
           <Route path="/reglesurb" element={<ReglesUrb />} />
-
         </Routes>
-      </Router> 
+      </Router>
     </div>
   );
 }
