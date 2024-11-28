3099e245039422f315a8e0b87c73ca0bbcf41646
chrissergio96 <safou.christiansergio@gmail.com>
2024-11-28 17:27:35 +0100

    Update AutorisationsUrb.jsx

--- src/ComponentsAutorisationsUrb/AutorisationsUrb/AutorisationsUrb.jsx
+++ src/ComponentsAutorisationsUrb/AutorisationsUrb/AutorisationsUrb.jsx
@@ -14,16 +14,14 @@ const AutorisationsUrb= () => {
             
            <div className='texte3a'>
             <p>
-<<<<<<< HEAD
+
             Bienvenue dans la rubrique Autorisations d'Urbanisme de la Mairie du 2ᵉ Arrondissement de Port-Gentil. Ce service est
 destiné à accompagner les citoyens et les professionnels dans leurs projets de construction ou de rénovation. Vous
 trouverez ici toutes les informations nécessaires sur les démarches à suivre pour obtenir un permis de construire,
 une déclaration préalable de travaux, un permis d’aménager, ou tout autre type d’autorisation d’urbanisme. Que vous
 souhaitiez construire, rénover, ou aménager, notre équipe est à votre disposition pour vous guider dans vos demandes
 et s’assurer que vos projets respectent les règles d'urbanisme en vigueur.
-=======
-                Bienvenue dans la rubrique aménagements de la Mairie du 2e Arrondissement de Port-Gentil. Ici vous trouverez toutes les informations sur les projets de develeppement urbain et d'infrastructure de notre Arrondissement. L'Aménagement de notre territoire vise à améliorer la qualité de vie de nos citoyens tout en respectant l'Environnement. Que ce soit la création de nouveaux espaces publics, la réhabilitation des infrastructures existantes, ou l'intégration de solutions durables, nous nous engageons à transformer notre cadre de vie pour le bien-etre de tous. Découvrez les projets en cours, les plans futurs, et comment ils contribuent à la modernisation de notre Arrondissement.
->>>>>>> origin/main
+
             </p>
            </div>
     </div>
