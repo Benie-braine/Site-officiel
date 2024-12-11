5c15122ab5fd957fba6849533e6b47d5bf522953
chrissergio96 <safou.christiansergio@gmail.com>
2024-12-11 11:16:16 +0100

    finturb

--- src/Pages/TravauxUrbanisme.jsx
+++ src/Pages/TravauxUrbanisme.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import NavbarTravauxUrb from '../ComponentsTravauxUrb/NavbarTravauxUrb/NavbarTravauxUrb';
 import HeaderTravauxUrb from '../ComponentsTravauxUrb/HeaderTravauxUrb/HeaderTravauxUrb';
-import TitreTravauxUrb from '../ComponentsTravauxUrb/TitreTravauxUrb/TitreTravauxUrb'
-
+import TitreTravauxUrb from '../ComponentsTravauxUrb/TitreTravauxUrb/TitreTravauxUrb';
+import TitreEncours from '../ComponentsTravauxUrb/TitreEncours/TitreEncours';
+import TitreAvenir from '../ComponentsTravauxUrb/TitreAvenir/TitreAvenir';
+import Boutonturb from '../ComponentsTravauxUrb/Boutonturb/Boutonturb';
+import FooterTravauxUrb from '../ComponentsTravauxUrb/FooterTravauxUrb/FooterTravauxUrb';
+import FooterbasTravauxUrb from '../ComponentsTravauxUrb/FooterbasTravauxUrb/FooterbasTravauxUrb';
 
 function TravauxUrbanisme() {
     return (
@@ -11,6 +15,12 @@ function TravauxUrbanisme() {
        <NavbarTravauxUrb/>  
        <HeaderTravauxUrb/>
        <TitreTravauxUrb/>
+       <TitreEncours/>
+       <TitreAvenir/>
+       <Boutonturb/>
+       <FooterTravauxUrb/>
+       <FooterbasTravauxUrb/>
+
       </div>
       
     );
