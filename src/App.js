cbca3f5a59c163aa84884cb22b47594de01beff8
unknown <birinda>
2024-12-18 16:56:55 +0100

    rout

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Urbanisme from './Pages/Urbanisme.jsx';
 import Amenagements from './Pages/Amenagements.jsx';
 import ReglesUrb from './Pages/ReglesUrb.jsx';
 import Transcription from './Pages/Transcription.jsx';
-// import Galerie from './Pages/Galerie.jsx'
-// import Service from './Pages/Service.jsx'
-// import Galeriemariage from './Pages/Galeriemariage.jsx';
-// import Cdqgalerie from './Cdqgalerie/Cdqheadergal/Cdqheadergal.js';
-// import Socialgalerie from './Pages/Socialgalerie.jsx';
-// import Culturegalerie from './Pages/Culturegalerie.jsx'
-// import Dossiercdq1 from './Pages/Dossiercdq1.jsx';
-// import Agenda from './Pages/Agenda.jsx'
+import Galerie from './Pages/Galerie.jsx'
+import Service from './Pages/Service.jsx'
+import Galeriemariage from './Pages/Galeriemariage.jsx';
+import Cdqgalerie from './Cdqgalerie/Cdqheadergal/Cdqheadergal.js';
+import Socialgalerie from './Pages/Socialgalerie.jsx';
+import Culturegalerie from './Pages/Culturegalerie.jsx'
+import Dossiercdq1 from './Pages/Dossiercdq1.jsx';
+import Agenda from './Pages/Agenda.jsx'
 import Légalisation from './Pages/Légalisation'; // Garder cette ligne
 import AutorisationsUrbanisme from './Pages/AutorisationsUrbanisme.jsx';
 import TravauxUrbanisme from './Pages/TravauxUrbanisme.jsx';
@@ -61,14 +61,14 @@ function App() {
 
           <Route path="/Cdq" element={<Cdq/>} />
 
-          {/* <Route path="/Galerie" element={<Galerie/>} />
+          <Route path="/Galerie" element={<Galerie/>} />
           <Route path="/Service" element={<Service/>} />
           <Route path="/Galeriemariage" element={<Galeriemariage/>} />
           <Route path="/Cdqgalerie" element={<Cdqgalerie/>} />
           <Route path="/Socialgalerie" element={<Socialgalerie/>} />
           <Route path="/Culturegalerie" element={<Culturegalerie/>} />
           <Route path="/Dossiercdq1" element={<Dossiercdq1/>} />
-          <Route path="/Agenda" element={<Agenda/>} /> */}
+          <Route path="/Agenda" element={<Agenda/>} />
 
 
 
